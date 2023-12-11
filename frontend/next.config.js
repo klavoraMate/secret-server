a0c17ce474779d981b7882c80d093dfcf07a94c8
klavoraMate <klavoramate98@gmail.com>
2023-12-11 12:33:20 +0100

    feat: added proxy and test fetch call

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://server-k1y7.onrender.com/api/:path*',
+      },
+    ]
+  },
+}
