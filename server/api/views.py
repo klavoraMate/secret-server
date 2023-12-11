319b4b58927ebb6f842911957a5fbab6b35cf1eb
klavoraMate <klavoramate98@gmail.com>
2023-12-11 14:35:41 +0100

    refactor: changed code according the PEP8 style guide

--- server/api/views.py
+++ server/api/views.py
@@ -1,8 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+
 @api_view(['GET'])
-def getData(request):
+def get_data(request):
     person = {
         "name": "John Doe",
         "age": 29,
