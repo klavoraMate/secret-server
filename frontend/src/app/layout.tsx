e8b2723a7ae330131c565f3e5d3fe47084dc5931
klavoraMate <klavoramate98@gmail.com>
2023-12-13 17:27:01 +0100

    feat: added background img

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import './globals.css'
 import NavBar from "@/app/components/NavBar";
+import Image from "next/image";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -14,8 +15,11 @@ export default function RootLayout({children,}: {children: React.ReactNode }) {
     return (
         <html lang="en">
             <body className={inter.className}>
-            <NavBar/>
-            {children}
+            <Image src='/background.jpg' alt={'background'} layout={'fill'} objectFit={'cover'}/>
+            <div style={{position: 'relative', zIndex: 1}}>
+                <NavBar/>
+                {children}
+            </div>
             </body>
         </html>
     )
