074aadcf1bf1eb8c180db79c45c5625a0442f230
klavoraMate <klavoramate98@gmail.com>
2023-12-13 15:34:43 +0100

    refactor: removed test fetch call

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,4 @@
-'use client';
-import {useEffect} from "react";
-
 export default function Home() {
-    useEffect(() => {
-        fetch('/api')
-            .then(res => res.json())
-            .then(res => console.log(res))
-    }, [])
 
     return (<>
     </>)
