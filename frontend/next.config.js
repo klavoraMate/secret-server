b22372e045c481af679b42616d9cdf42c16dae51
klavoraMate <klavoramate98@gmail.com>
2023-12-14 13:33:34 +0100

    try fixing  vercel issue problem 2

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,3 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    rewrites: async () => [
-        {
-            source: '/v1/:path*',
-            destination: 'https://server-k1y7.onrender.com/v1/:path*',
-        },
-    ],
 }
