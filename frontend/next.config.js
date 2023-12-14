fb2993070be62666f9e852ea624914551d298f7e
klavoraMate <klavoramate98@gmail.com>
2023-12-14 19:11:04 +0100

    update: rewrite and CORS moved to vercel.json

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    async headers() {
-        return [
-            {
-                source: "/v1/:path*",
-                headers: [
-                    {key: "Access-Control-Allow-Credentials", value: "true"},
-                    {key: "Access-Control-Allow-Origin", value: "https://server-k1y7.onrender.com"},
-                    {key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT"},
-                    {
-                        key: "Access-Control-Allow-Headers",
-                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-                    },
-                ]
-            }
-        ]
-    }
+
 }
