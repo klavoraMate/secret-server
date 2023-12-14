69899ecf425967054b349215460c23ee1ef61611
klavoraMate <klavoramate98@gmail.com>
2023-12-14 12:52:20 +0100

    update: fetch url routing issue

--- frontend/src/app/components/FormCreateSecret.tsx
+++ frontend/src/app/components/FormCreateSecret.tsx
@@ -14,7 +14,7 @@ export default function FormCreateSecret() {
         formData.append('secret', secret);
         formData.append('expireAfterViews', views);
         formData.append('expireAfter', time);
-        fetch('/v1/secret', {
+        fetch('https://server-k1y7.onrender.com/v1/secret', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
