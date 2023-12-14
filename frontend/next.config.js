56d0243ab9d87976d782e03e31d445a9c1256b50
klavoraMate <klavoramate98@gmail.com>
2023-12-14 13:01:55 +0100

    update: fetch url routing issue 2

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+    rewrites: async () => [
+        {
+            source: '/v1/:path*',
+            destination: 'https://server-k1y7.onrender.com/v1/:path*',
+        },
+    ],
 }
