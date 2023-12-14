47ffd1ce290a987c40ed406a303745f1462d3908
klavoraMate <klavoramate98@gmail.com>
2023-12-14 09:38:40 +0100

    feat: adjusted Home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,26 @@
-export default function Home() {
+'use client';
+import {Typography} from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
-    return (<>
-    </>)
+const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+    },
+    title: {
+        fontSize: '3rem',
+        fontWeight: 'bold',
+    },
+});
+
+export default function Home() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <Typography className={classes.title}>Secret Server</Typography>
+        </div>
+    );
 }
