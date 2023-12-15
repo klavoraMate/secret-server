f2eeb53dc6ff3f0f4d26fbb7f36c46143a706ffd
klavoraMate <klavoramate98@gmail.com>
2023-12-15 17:43:19 +0100

    update: dialog

---
+++ frontend/src/app/components/dialog/DialogContainer.tsx
@@ -0,0 +1,63 @@
+import {Dialog, DialogTitle} from "@mui/material";
+import {makeStyles} from "@mui/styles";
+
+interface DialogContainerProps {
+    open: boolean;
+    onClose: () => void;
+    title: string;
+    children: React.ReactNode;
+}
+export default function DialogContainer({open, onClose, title, children}: DialogContainerProps){
+    const classes = useStyle();
+
+    return (
+        <div>
+            <Dialog open={open} onClose={onClose} className={classes.root}>
+                <DialogTitle>{title}</DialogTitle>
+                    {children}
+            </Dialog>
+        </div>
+    );
+}
+const useStyle = makeStyles({
+    root: {
+        '& .MuiDialog-paper': {
+            backgroundColor: 'rgb(244, 133, 49)',
+            color: 'white',
+            borderRadius: '2rem',
+            padding: '2rem',
+            border: '2px solid white',
+            '& .MuiDialogTitle-root': {
+                fontSize: '2rem',
+                textAlign: 'center',
+                '& .MuiTypography-root': {
+                    fontFamily: 'sans-serif',
+                    fontWeight: 'bold',
+                },
+            },
+            '& .MuiDialogContent-root': {
+                fontSize: '1.5rem',
+                textAlign: 'center',
+                '& .MuiDialogContentText-root': {
+                    fontFamily: 'sans-serif',
+                },
+            },
+            '& .MuiDialogActions-root': {
+                justifyContent: 'center',
+                '& .MuiButton-root': {
+                    fontSize: '1.5rem',
+                    fontFamily: 'sans-serif',
+                    color: 'white',
+                    border: '2px solid white',
+                    borderRadius: '2rem',
+                    padding: '1rem 2rem',
+                    margin: '0 0.5rem',
+                    backgroundColor: 'rgb(244, 133, 49)',
+                    '&:hover': {
+                        backgroundColor: 'rgb(244, 133, 49, 0.5)',
+                    },
+                },
+            },
+        },
+    },
+});
--- frontend/src/app/create/page.tsx
+++ frontend/src/app/create/page.tsx
@@ -1,15 +1,24 @@
 'use client';
 import {useState} from "react";
-import {Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button} from "@mui/material";
+import {
+    DialogContent,
+    DialogContentText,
+    DialogActions,
+    Button,
+    Grid, Input
+} from "@mui/material";
 import FormCreateSecret from "@/app/components/form/FormCreateSecret";
+import DialogContainer from "@/app/components/dialog/DialogContainer";
+import {makeStyles} from "@mui/styles";
 
 export default function Create() {
     const [open, setOpen] = useState(false);
-    const [hash, setHash] = useState('');
+    const [response, setResponse] = useState('');
     const [isError, setIsError] = useState(false);
+    const classes = useStyle();
 
-    const handleResponse = (responseHash: string, error = false) => {
-        setHash(responseHash);
+    const handleResponse = (response: string, error = false) => {
+        setResponse(response);
         setIsError(error);
         setOpen(true);
     }
@@ -18,20 +27,46 @@ export default function Create() {
         setOpen(false);
     }
 
+    const handleCopy = () => {
+        navigator.clipboard.writeText(response);
+    }
+
     return (
         <div>
             <FormCreateSecret onResponse={handleResponse}/>
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>{isError ? "Error" : "Secret Created"}</DialogTitle>
+            <DialogContainer open={open} onClose={handleClose} title={isError ? 'Error:' : 'Secret created'}>
                 <DialogContent>
                     <DialogContentText>
-                        {isError ? hash : `The hash of your secret is ${hash}`}
+                        {isError ? response : 'The hash of your secret is'}
                     </DialogContentText>
+                    {!isError && (
+                        <Grid container alignItems="center" spacing={1}>
+                            <Grid item xs={8}>
+                                <Input value={response} readOnly
+                                       inputProps={{className: classes.input}}/>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <img src="/copied_icon.svg" alt="Copy" onClick={handleCopy} className={classes.image}/>
+                            </Grid>
+                        </Grid>
+                    )}
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose}>Close</Button>
                 </DialogActions>
-            </Dialog>
+            </DialogContainer>
         </div>
     );
 }
+
+const useStyle = makeStyles({
+    input: {
+        textAlign: 'center',
+        width: '100%',
+    },
+    image: {
+        marginTop: '0.5rem',
+        cursor: 'pointer',
+        width: '40%',
+    }
+});
