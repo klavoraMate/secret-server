6faf418a0fae37f87e4bc29ce85079b0fe55131c
klavoraMate <klavoramate98@gmail.com>
2023-12-15 18:58:55 +0100

    feat: implemented Dialog for the find secret page

--- frontend/src/app/components/form/FormFindSecret.tsx
+++ frontend/src/app/components/form/FormFindSecret.tsx
@@ -5,11 +5,33 @@ import FormInputField from "@/app/components/form/FormInputField";
 import {useState} from "react";
 import FormButton from "@/app/components/form/FormButton";
 
-export default function FormFindSecret() {
+
+interface FormFindSecretProps {
+    setData: (data: any) => void;
+    setError: (error: string | null) => void;
+}
+
+export default function FormFindSecret({setData, setError}: FormFindSecretProps) {
     const [hash, setHash] = useState<string>('');
-    const handleSubmit = () => {
-        fetch('/v1/secret/' + hash).then(response => response.json())
-    }
+
+    const fetchData = async () => {
+        try {
+            const resJson = await fetch('your-api-url', {headers: {Accept: 'application/json'}});
+            const resXml = await fetch('your-api-url', {headers: {Accept: 'application/xml'}});
+
+            if (resJson.status === 404 || resXml.status === 404) {
+                const dataJson = await resJson.json();
+                setError(dataJson.message);
+            } else {
+                const dataJson = await resJson.json();
+                const dataXml = await resXml.text();
+                setData({json: dataJson, xml: dataXml});
+            }
+
+        } catch (error: any) {
+            setError(error.message);
+        }
+    };
     return (
         <div>
             <FormContainer logoSrc={'/logo_form_find.png'} logoAlt={'find logo'} logoTitle={'Find secret text!'}>
@@ -18,7 +40,7 @@ export default function FormFindSecret() {
                                     onChange={(e) => setHash(e.target.value)} helperText={'Enter hash'}/>
                 </Grid>
                 <Grid item xs={12} container justifyContent='right'>
-                    <FormButton disabled={false} text={'Find'} onClick={handleSubmit}/>
+                    <FormButton disabled={false} text={'Find'} onClick={fetchData}/>
                 </Grid>
             </FormContainer>
         </div>
--- frontend/src/app/find/page.tsx
+++ frontend/src/app/find/page.tsx
@@ -1,9 +1,79 @@
-import FormFindSecret from "@/app/components/form/FormFindSecret";
+'use client';
+import {useState} from 'react';
+import {Dialog, Tab, Tabs, Box, Typography} from '@mui/material';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import {solarizedlight} from 'react-syntax-highlighter/dist/esm/styles/prism';
+import FormFindSecret from '@/app/components/form/FormFindSecret';
+
+interface DataProps {
+    json: {
+        hash: string;
+        secretText: string;
+        createdAt: string;
+        expiresAt: string;
+        remainingViews: number;
+    };
+    xml: string;
+}
 
 export default function Find() {
+    const [data, setData] = useState<DataProps>({} as DataProps);
+    const [error, setError] = useState<string | null>(null);
+    const [tabValue, setTabValue] = useState<number>(0);
+
+    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+        setTabValue(newValue);
+    };
+
     return (
         <div>
-            <FormFindSecret />
+            <FormFindSecret setData={setData} setError={setError}/>
+            <Dialog open={Boolean(data || error)} onClose={() => {
+                setData({} as DataProps);
+                setError(null);
+            }}>
+                {error && (
+                    <Typography variant="h6" color="error">{error}</Typography>
+                )}
+                {data.json && (<>
+                        <Tabs value={tabValue} onChange={handleTabChange}>
+                            <Tab label="Normal"/>
+                            <Tab label="JSON"/>
+                            <Tab label="XML"/>
+                        </Tabs>
+                        <TabPanel value={tabValue} index={0}>
+                            <Typography>Hash: {data.json.hash}</Typography>
+                            <Typography>Secret Text: {data.json.secretText}</Typography>
+                            <Typography>Created At: {data.json.createdAt}</Typography>
+                            <Typography>Expires At: {data.json.expiresAt}</Typography>
+                            <Typography>Remaining Views: {data.json.remainingViews}</Typography>
+                        </TabPanel>
+                        <TabPanel value={tabValue} index={1}>
+                            <SyntaxHighlighter language="json" style={solarizedlight}>
+                                {JSON.stringify(data.json, null, 2)}
+                            </SyntaxHighlighter>
+                        </TabPanel>
+                        <TabPanel value={tabValue} index={2}>
+                            <SyntaxHighlighter language="xml" style={solarizedlight}>
+                                {data.xml}
+                            </SyntaxHighlighter>
+                        </TabPanel></>
+                )}
+            </Dialog>
+        </div>
+    );
+}
+
+function TabPanel(props: { children: React.ReactNode; index: number; value: number; }) {
+    const {children, value, index, ...other} = props;
+
+    return (
+        <div role="tabpanel" hidden={value !== index} {...other}>
+            {value === index && (
+                <Box>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
         </div>
     );
 }
