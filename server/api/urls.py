88a83d3edf7b0905027157400b6e46f451aee2a4
klavoraMate <klavoramate98@gmail.com>
2023-12-17 20:17:36 +0100

    fix: removed unnecessary / on the end of the api url

--- server/api/urls.py
+++ server/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('secret/', views.create_secret, name='create_secret'),
-    path('secret/<str:hash_value>/', views.get_secret, name='get_secret'),
+    path('secret', views.create_secret, name='create_secret'),
+    path('secret/<str:hash_value>', views.get_secret, name='get_secret'),
 ]
