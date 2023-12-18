431767be482edae617161d253d8a4d962f72cf3d
klavoraMate <klavoramate98@gmail.com>
2023-12-18 12:33:57 +0100

    doc: FormCreateSecret

--- frontend/src/app/components/form/FormCreateSecret.tsx
+++ frontend/src/app/components/form/FormCreateSecret.tsx
@@ -5,12 +5,30 @@ import FormInputField from "@/app/components/form/FormInputField";
 import FormButton from "@/app/components/form/FormButton";
 import FormContainer from "@/app/components/form/FormContainer";
 
-export default function FormCreateSecret({onResponse}: { onResponse: (hash: string, error?: boolean) => void }) {
+/**
+ * @interface FormCreateSecretProps
+ * @property {Function} onResponse - Function to be called when the form submission is completed.
+ */
+interface FormCreateSecretProps {
+    onResponse: (hash: string, error?: boolean) => void;
+}
+
+/**
+ * FormCreateSecret is a component that renders a form for creating a secret.
+ * @param {FormCreateSecretProps} props - The properties that define the form's behavior.
+ * @returns {JSX.Element} The FormCreateSecret component.
+ */
+export default function FormCreateSecret({onResponse}: FormCreateSecretProps) {
     const [secret, setSecret] = useState<string>('');
     const [views, setViews] = useState<string>('');
     const [time, setTime] = useState<string>('');
     const [loading, setLoading] = useState<boolean>(false);
 
+    /**
+     * Handles the form submission.
+     * Sends a POST request to the '/v1/secret' endpoint with the form data.
+     * Calls the onResponse prop function with the response data.
+     */
     const handleSubmit = () => {
         setLoading(true);
         const formData = new URLSearchParams();
@@ -63,4 +81,3 @@ export default function FormCreateSecret({onResponse}: { onResponse: (hash: stri
         </div>
     );
 }
-
