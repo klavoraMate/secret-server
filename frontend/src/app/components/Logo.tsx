7634b4f0ba0fca96fd98cf29f7cf7c242d8fc74a
klavoraMate <klavoramate98@gmail.com>
2023-12-18 12:46:12 +0100

    doc: Logo

--- frontend/src/app/components/Logo.tsx
+++ frontend/src/app/components/Logo.tsx
@@ -2,12 +2,23 @@ import Box from "@mui/material/Box";
 import React from "react";
 import Typography from "@mui/material/Typography";
 
+/**
+ * @interface LogoProps
+ * @property {string} title - The title to be displayed below the logo image.
+ * @property {string} src - The source URL of the logo image.
+ * @property {string} alt - The alt text for the logo image.
+ */
 interface LogoProps {
     title: string;
     src: string;
     alt: string;
 }
 
+/**
+ * Logo is a component that renders a logo image and a title.
+ * @param {LogoProps} props - The properties that define the logo's source, alt text, and title.
+ * @returns {JSX.Element} The Logo component.
+ */
 export default function Logo({title, src, alt}: LogoProps) {
     return (
         <div>
@@ -24,11 +35,20 @@ export default function Logo({title, src, alt}: LogoProps) {
         </div>
     )
 }
+
+/**
+ * The styles for the Box component in the Logo component.
+ * @type {Object}
+ */
 const boxStyles = {
     display: 'flex',
     justifyContent: 'space-around',
 }
 
+/**
+ * The styles for the Typography component in the Logo component.
+ * @type {Object}
+ */
 const titleStyles = {
     textAlign: 'center',
     marginTop: '2rem',
