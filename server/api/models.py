aac3bf2e6ab49b5b990d40d7a43f30be646a5f15
klavoraMate <klavoramate98@gmail.com>
2023-12-18 11:24:29 +0100

    doc: models.py

--- server/api/models.py
+++ server/api/models.py
@@ -5,6 +5,16 @@ import hashlib
 
 
 class Secret(models.Model):
+    """Secret is a Django model that represents a secret entity in the database.
+
+    Attributes:
+    hash (str): The hash of the secret.
+    secret_text (EncryptedField): The secret text, stored in an encrypted format.
+    created_at (datetime): The time when the secret was created.
+    expires_at (datetime): The time when the secret will expire.
+    remaining_views (int): The number of remaining views for the secret.
+    """
+
     hash = models.CharField(max_length=255)
     secret_text = EncryptedField()
     created_at = models.DateTimeField()
@@ -13,6 +23,16 @@ class Secret(models.Model):
 
     @classmethod
     def create_secret(cls, secret, expire_after_views, expire_after):
+        """This class method is used to create a new secret.
+
+        Parameters:
+        secret (str): The secret text.
+        expire_after_views (int): The number of views after which the secret will expire.
+        expire_after (int): The number of minutes after which the secret will expire.
+
+        Returns:
+        Secret: A new Secret object.
+        """
         created_at = datetime.now()
         expires_at = created_at + timedelta(minutes=expire_after)
 
@@ -24,4 +44,3 @@ class Secret(models.Model):
                                   created_at=created_at,
                                   expires_at=expires_at,
                                   remaining_views=expire_after_views)
-
