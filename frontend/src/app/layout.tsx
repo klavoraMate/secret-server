8d732a690f59425ddb3e14d7918097ca3bc714b2
klavoraMate <klavoramate98@gmail.com>
2023-12-18 13:02:24 +0100

    doc: find layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import './globals.css'
 import NavBar from "@/app/components/navigation/NavBar";
-import Image from "next/image";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -11,15 +10,22 @@ export const metadata: Metadata = {
     description: 'Site for storing and retrieving secrets',
 }
 
+/**
+ * RootLayout is a component that renders the root layout of the application.
+ * It includes a navigation bar and the child components passed to it.
+ * @param {Object} props - The properties passed to the component.
+ * @param {React.ReactNode} props.children - The child components to be rendered inside the root layout.
+ * @returns {JSX.Element} The RootLayout component.
+ */
 export default function RootLayout({children,}: {children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={inter.className}>
-            <div style={{position: 'relative', zIndex: 1}}>
-                <NavBar/>
-                {children}
-            </div>
-            </body>
+        <body className={inter.className}>
+        <div style={{position: 'relative', zIndex: 1}}>
+            <NavBar/>
+            {children}
+        </div>
+        </body>
         </html>
     )
 }
