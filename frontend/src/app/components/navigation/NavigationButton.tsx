33b0831f8bf3ecde7d6a43b5f8aac1d187e56bb0
klavoraMate <klavoramate98@gmail.com>
2023-12-18 12:44:51 +0100

    doc: NavigationButton

--- frontend/src/app/components/navigation/NavigationButton.tsx
+++ frontend/src/app/components/navigation/NavigationButton.tsx
@@ -2,11 +2,21 @@
 import Button from "@mui/material/Button";
 import {useRouter} from "next/navigation";
 
+/**
+ * @interface NavigationButtonProps
+ * @property {string} text - The text to be displayed on the button.
+ * @property {string} link - The URL to navigate to when the button is clicked.
+ */
 interface NavigationButtonProps {
     text: string;
     link: string;
 }
 
+/**
+ * NavigationButton is a component that renders a button which navigates to a specified URL when clicked.
+ * @param {NavigationButtonProps} props - The properties that define the button's behavior and appearance.
+ * @returns {JSX.Element} The NavigationButton component.
+ */
 export default function NavigationButton({text, link}: NavigationButtonProps) {
     const router = useRouter();
 
@@ -17,15 +27,19 @@ export default function NavigationButton({text, link}: NavigationButtonProps) {
     );
 }
 
-    const buttonStyles= {
-        fontSize: '1.5rem',
-        color: 'white',
-        margin: '0 0.5rem',
-        background: 'transparent',
-        border: '2px solid white',
-        fontStyle: 'sans-serif',
-        '&:hover': {
-            backgroundColor: 'white',
-            color: 'black',
-        },
-    }
+/**
+ * The styles for the NavigationButton component.
+ * @type {Object}
+ */
+const buttonStyles= {
+    fontSize: '1.5rem',
+    color: 'white',
+    margin: '0 0.5rem',
+    background: 'transparent',
+    border: '2px solid white',
+    fontStyle: 'sans-serif',
+    '&:hover': {
+        backgroundColor: 'white',
+        color: 'black',
+    },
+}
