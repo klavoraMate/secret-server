69c7c86e4814e12bbf9eec9fbf5bc3cd215c37a6
klavoraMate <klavoramate98@gmail.com>
2023-12-18 12:40:13 +0100

    doc: FormInputField

--- frontend/src/app/components/form/FormInputField.tsx
+++ frontend/src/app/components/form/FormInputField.tsx
@@ -1,6 +1,14 @@
 import {FormHelperText, TextField} from "@mui/material";
 import {styled} from "@mui/system";
 
+/**
+ * @interface FormInputFieldProps
+ * @property {string} id - The id of the input field.
+ * @property {string} type - The type of the input field.
+ * @property {string} value - The current value of the input field.
+ * @property {Function} onChange - Function to be called when the input field's value changes.
+ * @property {string} helperText - The helper text to be displayed below the input field.
+ */
 interface FormInputFieldProps {
     id: string;
     type: string;
@@ -9,6 +17,40 @@ interface FormInputFieldProps {
     helperText: string;
 }
 
+
+
+/**
+ * FormInputField is a component that renders an input field with custom styles which is used in forms.
+ * @param {FormInputFieldProps} props - The properties that define the input field's behavior and appearance.
+ * @returns {JSX.Element} The FormInputField component.
+ */
+export default function FormInputField({id, type, value, onChange, helperText}: FormInputFieldProps) {
+
+    return (
+        <div>
+            <FormHelperText
+                id={`${id}-helper-text`}
+                sx={helperTextStyles}
+            >
+                {helperText}
+            </FormHelperText>
+            <TextFieldStyled
+                id={id}
+                aria-describedby={`${id}-helper-text`}
+                type={type}
+                value={value}
+                onChange={onChange}
+                variant={'outlined'}
+            />
+        </div>
+    );
+
+}
+
+/**
+ * The styles for the TextField component in the FormInputField component.
+ * @type {Object}
+ */
 const TextFieldStyled = styled(TextField)({
     '& .MuiInputBase-root': {
         color: 'white',
@@ -39,30 +81,10 @@ const TextFieldStyled = styled(TextField)({
     },
 });
 
-
-export default function FormInputField({id, type, value, onChange, helperText}: FormInputFieldProps) {
-
-    return (
-        <div>
-            <FormHelperText
-                id={`${id}-helper-text`}
-                sx={helperTextStyles}
-            >
-                {helperText}
-            </FormHelperText>
-            <TextFieldStyled
-                id={id}
-                aria-describedby={`${id}-helper-text`}
-                type={type}
-                value={value}
-                onChange={onChange}
-                variant={'outlined'}
-            />
-        </div>
-    );
-
-}
-
+/**
+ * The styles for the FormHelperText component in the FormInputField component.
+ * @type {Object}
+ */
 const helperTextStyles = {
     fontSize: '1.2rem',
     marginBottom: '0.5rem',
@@ -70,5 +92,3 @@ const helperTextStyles = {
     color: 'white',
     fontFamily: 'monospace',
 }
-
-
